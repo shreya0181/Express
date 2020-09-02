6c8f6f653c023196ae83c1956928dbb2b9d777f9
shreya0181 <shreya171769.cse@chitkara.edu.in>
2020-09-02 18:47:12 +0530

    Cookies

--- index.js
+++ index.js
@@ -13,40 +13,43 @@ const Dishes = require('./models/dishes');
 const URL= 'mongodb://localhost:27017/conFusion';
 const connect = mongoose.connect(URL);
 
-function auth(req, res, next)
-{
-  console.log(req.headers);
+app.use(cookieParser('12345-67890-09876-54321'));
 
-  var authHeader = req.headers.authorization;
+function auth (req, res, next) {
 
-  if(!authHeader)
-  {
-    var err = new Error('You are not authenticated');
-
-    res.setHeader('WWW-Authenticate', 'Basic');
-    err.status=401;
-    return next(err);
+  if (!req.signedCookies.user) {
+    var authHeader = req.headers.authorization;
+    if (!authHeader) {
+        var err = new Error('You are not authenticated!');
+        res.setHeader('WWW-Authenticate', 'Basic');              
+        err.status = 401;
+        next(err);
+        return;
+    }
+    var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
+    var user = auth[0];
+    var pass = auth[1];
+    if (user == 'admin' && pass == 'password') {
+        res.cookie('user','admin',{signed: true});
+        next(); // authorized
+    } else {
+        var err = new Error('You are not authenticated!');
+        res.setHeader('WWW-Authenticate', 'Basic');              
+        err.status = 401;
+        next(err);
+    }
   }
-
-  var auth = new Buffer(authHeader.split(' ')[1], 'base64').toString.split(':');
-  var username =auth[0];
-  var password= auth[1];
-
-  if( username=== 'admin' && password ==='password')
-  {
-    next();
+  else {
+      if (req.signedCookies.user === 'admin') {
+          next();
+      }
+      else {
+          var err = new Error('You are not authenticated!');
+          err.status = 401;
+          next(err);
+      }
   }
-
-  else{
-    var err = new Error('You are not authenticated');
-
-    res.setHeader('WWW-Authenticate', 'Basic');
-    err.status=401;
-    return next(err);
-  }
-
 }
-
 app.use(auth);
 
 
